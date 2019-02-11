b10d4221411ad094a72f36c3d2047a668030e44f
Michał <nowakowski.michal@hotmail.com>
2019-02-11 18:35:26 +0100

    Add local storage to keep random beer of day

--- src/components/RandomBeerofDay/RandomBeerofDay.js
+++ src/components/RandomBeerofDay/RandomBeerofDay.js
@@ -4,11 +4,32 @@ import "./_randomBeerofDay.scss";
 
 class RandomBeerofDay extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            objRandomBeer: this.getObjectBeer()
+        }
 
     }
 
     getRandom = (min, max) => Math.floor((Math.random() * (max - min) + min));
+
+    getObjectBeer = () => {
+        const beerFromStorage = JSON.parse(localStorage.getItem('beer'));
+
+
+        if (beerFromStorage) {
+            return beerFromStorage
+        } else {
+            const beerFromProps = this.props.beerRandom[this.getRandom(0, this.props.beerRandom.length - 1)]
+
+
+            localStorage.setItem('beer', JSON.stringify(beerFromProps))
+
+            return beerFromProps
+        }
+
+    }
+
 
     render() {
         const {beerRandom} = this.props;
@@ -17,7 +38,7 @@ class RandomBeerofDay extends React.Component {
             return null;
         } else {
             return <section className="random-beer">
-                <h2>Beer of day: <span>{beerRandom[this.getRandom(0, beerRandom.length - 1)].name}</span></h2>
+                <h2>Beer of day: <span>{this.state.objRandomBeer.name}</span></h2>
 
             </section>
         }
