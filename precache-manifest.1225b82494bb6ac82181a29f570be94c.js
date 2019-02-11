6bcc3233f7a3205907dfce7a5f7dd8ea048a9e32
Michał <nowakowski.michal@hotmail.com>
2019-02-11 13:15:29 +0100

    Updates

---
+++ precache-manifest.1225b82494bb6ac82181a29f570be94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db5867b1850b1096fe0496271d5318dd",
+    "url": "/your-beer-react-app/static/media/headerBackground.db5867b1.jpg"
+  },
+  {
+    "revision": "2a3c33a59eea118e89ad",
+    "url": "/your-beer-react-app/static/js/runtime~main.2a3c33a5.js"
+  },
+  {
+    "revision": "8fbcde6d7f12ff0c78db",
+    "url": "/your-beer-react-app/static/js/main.8fbcde6d.chunk.js"
+  },
+  {
+    "revision": "11dac3cdf202915bf55a",
+    "url": "/your-beer-react-app/static/js/1.11dac3cd.chunk.js"
+  },
+  {
+    "revision": "8fbcde6d7f12ff0c78db",
+    "url": "/your-beer-react-app/static/css/main.3a6a7886.chunk.css"
+  },
+  {
+    "revision": "13e88bdbd944dc5b864f72df1681b95e",
+    "url": "/your-beer-react-app/index.html"
+  }
+];
