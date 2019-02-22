c243b04806920201e8a4784ad85e8b9a92792bcf
Michał <nowakowski.michal@hotmail.com>
2019-02-22 17:22:06 +0100

    Updates

---
+++ precache-manifest.47e3d034d4f67ef64ca27a223c0141ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db5867b1850b1096fe0496271d5318dd",
+    "url": "/your-beer-react-app/static/media/headerBackground.db5867b1.jpg"
+  },
+  {
+    "revision": "2a3c33a59eea118e89ad",
+    "url": "/your-beer-react-app/static/js/runtime~main.2a3c33a5.js"
+  },
+  {
+    "revision": "c63533e3aa5975c80dcf",
+    "url": "/your-beer-react-app/static/js/main.c63533e3.chunk.js"
+  },
+  {
+    "revision": "ef8668c99b5a7927e1df",
+    "url": "/your-beer-react-app/static/js/1.ef8668c9.chunk.js"
+  },
+  {
+    "revision": "c63533e3aa5975c80dcf",
+    "url": "/your-beer-react-app/static/css/main.a3d6cc65.chunk.css"
+  },
+  {
+    "revision": "776117c28ebd59fa4d4be28ddde8657d",
+    "url": "/your-beer-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/your-beer-react-app/precache-manifest.b08c6e09504861a7fcd3ec606e6662d7.js"
+  "/your-beer-react-app/precache-manifest.47e3d034d4f67ef64ca27a223c0141ea.js"
 );
 
 workbox.clientsClaim();
